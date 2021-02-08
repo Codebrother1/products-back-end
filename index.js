7e46059b4393664d038a802ea0f0c9b5515e3bbd
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-02-08 16:40:31 -0600

    fix step 4

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 require("dotenv").config();
-
-const express = require('express')
-const massive = require('massive')
+const express = require("express");
+const massive = require("massive");
 
 const app = express();
 
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
-
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive({
-  connectionString: CONNECTION_STRING,
+connectionString: CONNECTION_STRING,
 ssl: {rejectUnauthorized: false}
-}).then(dbInstance => {
-  app.set('db', dbInstance)
-}).catch(err => console.log(err));
+})
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
 
-
-app.use(express.json())
-
+app.use(express.json());
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server lisitng on ${SERVER_PORT}.`);
-})
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
